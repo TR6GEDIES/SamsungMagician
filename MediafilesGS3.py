418f20ff7aa1279c881d81c68d826aa453f9050b
TR6GEDIES <141397608+TR6GEDIES@users.noreply.github.com>
2025-04-11 18:33:22 +0400

    MediafilesGS3.py

---
+++ MediafilesGS3.py
@@ -0,0 +1,237 @@
+__version__ = (1, 0, 3)
+
+# Name: MediafilesGS3
+# meta developer: @SamsungMagician (main developer @mqone)
+
+from .. import loader
+import asyncio
+
+@loader.tds
+class MediafilesGS3(loader.Module):
+    """Отправляет гс с канала https://t.me/MediafilesForModule"""
+
+    strings = {"name": "MediafilesGS3"}
+
+    # 1
+    async def мазякаcmd(self, message):
+        """— Негры все пидорасы"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/48",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 2
+    async def таджмахалcmd(self, message):
+        """— Горячий шлюшка на одно место"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/49",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 3
+    async def алахакбарcmd(self, message):
+        """— Тоже осуждаем"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/50",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 4
+    async def приветкакcmd(self, message):
+        """— Давай размножаться"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/51",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 5
+    async def харошбратcmd(self, message):
+        """— Сигма basedd"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/52",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 6
+    async def правилачатаcmd(self, message):
+        """— Правила чата (баян)"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/54",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 7
+    async def нускиньсиськиcmd(self, message):
+        """— Ну пожалуйста"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/55",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 8
+    async def добитьженщинуcmd(self, message):
+        """— Дота момент"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/56",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 9
+    async def кошечкаcmd(self, message):
+        """— Бедная кошечка"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/58",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 10
+    async def наколениcmd(self, message):
+        """— На колени на колени"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/60",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 11
+    async def никитаcmd(self, message):
+        """— Ты заебал"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/69",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 12
+    async def яйцаcmd(self, message):
+        """— Огромные яйца"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/70",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 13
+    async def добрыйcmd(self, message):
+        """— Добрый дядька"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/71",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 14
+    async def картаcmd(self, message):
+        """— Точно не развод"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/45",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 15
+    async def фимозcmd(self, message):
+        """— Онет фимоз тыщ тыщ"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/66",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+
+    # 16
+    async def обожаюcmd(self, message):
+        """— Когда раком меня"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/MediafilesForModule/84",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
