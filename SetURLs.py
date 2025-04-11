aabb53c62fab4ceec682bec2a0dbe5ddcb15d2e0
TR6GEDIES <141397608+TR6GEDIES@users.noreply.github.com>
2025-04-11 18:33:53 +0400

    SetURLs.py

---
+++ SetURLs.py
@@ -0,0 +1,103 @@
+__version__ = (1, 0, 0)
+
+# Name: SetURLs
+# meta developer: @SamsungMagician
+
+from .. import loader, utils
+import asyncio
+
+@loader.tds
+class SetURLs(loader.Module):
+    """Разработчик @SamsungMagician, URLs by https://t.me/FunSetUrls"""
+
+    strings = {"name": "SetURLs"}
+
+    async def stonycmd(self, message):
+        """— Стоны"""
+        lst = [f'<emoji document_id=5271604874419647061>🔗</emoji> <a href="https://deletelife.ru/pGe0IOftxT">Реферальная ссылка</a> <b>на 50 робуксов, от Павла Дурова</b>']
+        for i in lst:
+            await message.edit(i)
+
+    async def bosincmd(self, message):
+        """— Злодей Бо Синн"""
+        lst = [f'<emoji document_id=5287696755373124799>⚡️</emoji> <a href="https://m.4tube.com/videos/639800/hot-flasher-bo-sinn-fucks-dude-vote">Фулл</a> <b>Бо Синна</b>']
+        for i in lst:
+            await message.edit(i)
+            
+    async def leshcmd(self, message):
+        """— Ебалом не щелкай"""
+        lst = [f'<emoji document_id=5341621650322370106>🪟</emoji> <b>Анимка</b> <a href="http://eelslap.com/">лещём</a> по ебалу']
+        for i in lst:
+            await message.edit(i)
+            
+    async def jopacmd(self, message):
+        """— Холостой"""
+        lst = [f'<emoji document_id=6082293055484857290>🫥</emoji> <b>Анимка</b> <a href="https://www.misternicehands.com/">пустить холостого</a>']
+        for i in lst:
+            await message.edit(i)
+            
+    async def skorostrelcmd(self, message):
+        """— ХАХАХА ебать она лошара"""
+        lst = [f'<emoji document_id=5260219392859322932>🔫</emoji> <a href="https://qu.ax/SskN.mp4">Скорострел</a>']
+        for i in lst:
+            await message.edit(i)
+
+    async def belosnegcmd(self, message):
+        """— Советский мультфильм"""
+        lst = [f'<emoji document_id=5370862723976405092>❄️</emoji> <a href="https://mobi.huyamba.info/video/5015/">Белоснежка</a>']
+        for i in lst:
+            await message.edit(i)
+            
+    async def basedcmd(self, message):
+        """— Базовая база"""
+        lst = [f'<emoji document_id=6077697788370487612>👮‍♂️</emoji> <a href="https://www.tmz.com/watch/2023-03-28-032823-nashville-shooting-police-cam-alt-angle-1587935-399/">Based</a> <b>american police</b>']
+        for i in lst:
+            await message.edit(i)
+            
+    async def baitchelcmd(self, message):
+        """— Наебать"""
+        lst = [f'<emoji document_id=5267231042934154418>🟢</emoji> <b>Спам лс этому</b> <a href="tg://settings">челику</a>, <b>кидает дикпики в лс</b>']
+        for i in lst:
+            await message.edit(i)
+
+    async def dolphincmd(self, message):
+        """— None"""
+        lst = [f'<emoji document_id=5362063083311214432>🐬</emoji> <a href="https://ukdevilz.com/watch/-192289250_456239019">BelleDelphin</a>, <b>in the forest</b>']
+        for i in lst:
+            await message.edit(i)
+
+    async def delttcmd(self, message):
+        """— Удаление аккаунта в ТикТок ⚠️"""
+        lst = [f'<emoji document_id=5274074880046802038>🎁</emoji> <a href="https://vm.tiktok.com/ZMr2qdHbH">То самое видео из ТикТока...</a>']
+        for i in lst:
+            await message.edit(i)
+
+    async def voenkomcmd(self, message):
+        """— Звонок в центральный военкомат VK"""
+        lst = [f'<emoji document_id=5334853932915114338>📱</emoji> <a href="https://vk.com/call?id=585910193">Жми чтобы увидеть фулл...</a>']
+        for i in lst:
+            await message.edit(i)
+
+    async def light1cmd(self, message):
+        """— LightShot V1"""
+        lst = [f'<emoji document_id=5425113087252377803>🕐</emoji> <a href="https://parad1st.github.io/Screamer/">Реферальная ссылка</a> <b>на Telegram Premium 1 Month</b>']
+        for i in lst:
+            await message.edit(i)
+
+    async def light2cmd(self, message):
+        """— LightShot V2"""
+        lst = [f'<emoji document_id=5425113087252377803>🕐</emoji> <a href="https://youtube.ru.net/EmVOGziNbq">Реферальная ссылка</a> <b>на Telegram Premium 1 Month</b>']
+        for i in lst:
+            await message.edit(i)
+
+    async def light3cmd(self, message):
+        """— LightShot V3"""
+        lst = [f'<emoji document_id=5425113087252377803>🕐</emoji> <a href="https://web.archive.org/web/20210426233039/https://pnrtscr.com/4ci90a">Реферальная ссылка</a> <b>на Telegram Premium 1 Month</b>']
+        for i in lst:
+            await message.edit(i)
+
+    async def discordcmd(self, message):
+        """— Подпиши контракт"""
+        lst = [f'<emoji document_id=5325612636467903082>📱</emoji> Обычная <a href="https://discord.ru/">ссылка</a> на Discord...']
+        for i in lst:
+            await message.edit(i)
